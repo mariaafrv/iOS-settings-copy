f5ff1ba14b413cd512e3d19912003babe074ae97
Maria Aida Vitória <maria.vitoria@e-unicred.com.br>
2024-11-03 22:24:40 -0300

    add automatic lock screen

--- settings-copy/AutomaticLock.swift
+++ settings-copy/AutomaticLock.swift
@@ -5,12 +5,10 @@ struct AutomaticLock: View {
       List{
         Text("30 segundos")
         Text("1 minuto")
-        Text("30 segundos")
-        Text("1 minuto")
-        Text("30 segundos")
-        Text("1 minuto")
-        Text("30 segundos")
-        Text("1 minuto")
+        Text("3 minutos")
+        Text("4 minutos")
+        Text("5 minutos")
+        Text("Nunca")
       }.navigationTitle("Bloqueio Automático")
         .navigationBarTitleDisplayMode(.inline)
     }
